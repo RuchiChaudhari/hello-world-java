cfeba82a27e67d29ea10ce6ac98e191e173ff3c1
Ruchika Chaudhari <ruchichaudhari111@gmail.com>
2024-10-23 11:23:57 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+// java program to print hello world (single line comment)
+
+/*
+ * this is 
+ * multi line comment 
+ */
+
+ /** documentation */
+public class helloworld{
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
